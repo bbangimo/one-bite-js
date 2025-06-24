e2ecf553baa8d5e6f29ffcd6d68c95ca3f132487
alice <alice@uzen.net>
2025-06-24 21:22:07 +0900

    day02 mission

--- day02/index.js
+++ day02/index.js
@@ -1,5 +1,42 @@
 // mission1
-case 'tiger':
-    console.log('호랑이');
-    break;
-    
+
+let animal = 'tiger';
+
+switch (animal) {
+    case 'dog':
+        console.log('강아지');
+        break;
+
+    case 'tiger':
+        console.log('호랑이');
+        break;
+
+    case 'cat':
+        console.log('고양이');
+        break;
+
+    case 'lion':
+        console.log('사자');
+        break;
+
+    default:
+        console.log('다른 동물');
+        break;
+}
+
+// mission2
+let answer = '';
+
+function connectStrings(str1, str2) {
+    if (str1 === undefined) {
+        str1 = 'hello';
+    }
+    if (str2 === undefined) {
+        str2 = 'javascript';
+    }
+    answer = str1 + ' ' + str2;
+}
+
+connectStrings();
+
+console.log(answer);
